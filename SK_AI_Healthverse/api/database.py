a0ef7e229279b853647c1bbe8509d249c525b806
sknaveed38 <sknaveedsk20@gmail.com>
2026-06-20 19:25:54 +0530

    last update2

--- SK_AI_Healthverse/api/database.py
+++ SK_AI_Healthverse/api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 import datetime
 
 # SQLite database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./healthverse.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///tmp/healthverse.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
